dee4737651a50ae40d405bcd9327ba689e3ecf7e
shridharrai <shridharrai3@gmail.com>
2020-01-29 18:13:34 +0530

    sign-in Component without firebase integration

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from '../src/components/header/header.component';
+import SignInAndSignUp from '../src/components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
